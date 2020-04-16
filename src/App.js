a9fd13ea03caf869ccc34dc196c31328f6686437
Shivakumar Mangina <shiva2nani.mangina@gmail.com>
2020-04-16 10:30:23 +0530

    add routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import "./App.css";
+import BarChart from "./charts/BarChart"
+import LineChart from "./charts/LineChart"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div>
+        <Switch>
+          <Route path="/" component={BarChart} exact={true} />
+          <Route path="/bar-chart" component={BarChart} exact={true} />
+          <Route path="/line-chart" component={LineChart} exact={true} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
