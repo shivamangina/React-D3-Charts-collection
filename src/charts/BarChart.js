065e5a4a37ada53f7b777629f7a2b06c897c57ec
Shivakumar Mangina <shiva2nani.mangina@gmail.com>
2020-04-17 09:35:04 +0530

    D3 added and used within the component

--- src/charts/BarChart.js
+++ src/charts/BarChart.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react'
+import * as d3 from 'd3'
+class BarChart extends Component {
+    state = {
+        temperatureData : [ 8, 5, 13, 9, 12 ]
+    }
+    
+    componentDidMount(){
+       this.createChart()
+    }
 
-export default class BarChart extends Component {
-    render() {
-        return (
-            <div>
-                BarChart
-            </div>
-        )
+    createChart =  () => {
+        d3.select(this.refs.temperatures)
+        .selectAll("h2")
+        .data(this.state.temperatureData)
+        .enter()
+            .append("h2")
+            .text("New Temperature")
+    }
+    
+   
+   
+ 
+    render(){
+        return <div ref="temperatures"></div>
     }
 }
+export default BarChart
